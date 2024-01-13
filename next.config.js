9578e1151be0f8ce5626179d7c4054304cf814fd
sjyoung428 <sjy8267@naver.com>
2024-01-13 22:55:04 +0900

    랜딩 페이지

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        destination: "/i/flow/login",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
